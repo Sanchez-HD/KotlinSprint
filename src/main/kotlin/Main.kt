7f2d4ef3a6630e1960c353374fe690342fd51479
Sanchez-HD <Sanchez@>
2025-12-02 23:12:52 +0300

    Lesson 1, Task 1.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,6 @@ fun main() {
     val NumberOfOrders: Int = 75
     val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
 
-    println(NumberOfOrders)
-    println(thankYouMessage)
 
 
 
