c667c5c201c8c6339e8e0ab5dc098b03e738fb13
Sanchez-HD <Sanchez@>
2025-12-03 15:07:31 +0300

    Lesson 1, Task 2. "Remove extra file with tasks 1-3"

--- src/main/kotlin/lesson1/Lesson1-Task2.kt
+++ src/main/kotlin/lesson1/Lesson1-Task2.kt
@@ -1,14 +1,11 @@
 package lesson1
-
 fun main() {
-
     val numberOfOrders: Int = 75
     val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
-
     println(numberOfOrders)
     println(thankYouMessage)
-
-    //val stringValue: String = "Employee count 2000"
-    val stringValue: String = "Employee count 1999"
-    println(stringValue)
+    val stringValue1: String = "Employee count 2000"
+    //println(stringValue1)
+    val stringValue2: String = "Employee count 1999"
+    println(stringValue2)
 }
