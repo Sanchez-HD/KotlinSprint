160151d0a6f77577e8666de45c2993d54e14f77c
Sanchez-HD <Sanchez@>
2025-12-03 11:01:02 +0300

    Lesson 1, Task 2.

--- src/main/kotlin/lesson1/Lesson1-Task1.kt
+++ src/main/kotlin/lesson1/Lesson1-Task1.kt
@@ -1,4 +1,4 @@
-package org.example
+package lesson1
 
 fun main() {
 
---
+++ src/main/kotlin/lesson1/Lesson1-Task2.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    //val stringValue: String = "Employee count 2000"
+    val stringValue: String = "Employee count 1999"
+    println(stringValue)
+
+
+
+}
