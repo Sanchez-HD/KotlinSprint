86a4f3b8c16125c797ca7b6016b57e19047a0725
Sanchez-HD <Sanchez@>
2025-12-03 11:45:02 +0300

    Lesson 1, Task 2.

--- src/main/kotlin/lesson1/Lesson1-Task1.kt
+++ src/main/kotlin/lesson1/Lesson1-Task1.kt
@@ -2,6 +2,6 @@ package lesson1
 
 fun main() {
 
-    val NumberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
 }
