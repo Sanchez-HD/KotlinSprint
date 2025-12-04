a31d896380b0661bebcdf170cf5c473c55c7482b
Sanchez-HD <Sanchez@>
2025-12-04 14:36:18 +0300

    Lesson 1, Task 3

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main() {
+
+//  val year = 1961
+    val hour = "10:"
+    val time = 55
+//    println(year)
+//    println(hour)
+//    println(time)
+    print(hour)
+    print(time)
+}
+// Данные не изменяемые. Переменную var не используем?
