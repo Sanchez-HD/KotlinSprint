6257a21753701e166fb522ef70da3c207d872514
Sanchez-HD <Sanchez@>
2025-12-04 14:34:15 +0300

    Lesson 1, Task 2 Flow Refactoring

---
+++ src/main/kotlin/Lesson1_task2.kt
@@ -0,0 +1,11 @@
+package lesson1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    var numberOfWorkers: String = "Employee count 1999"
+    println(numberOfWorkers)
+}
