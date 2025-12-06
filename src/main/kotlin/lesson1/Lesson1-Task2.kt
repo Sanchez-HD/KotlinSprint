88a3074aeff34e270522e681f5b2c87519b21de5
Sanchez-HD <Sanchez@>
2025-12-06 11:00:33 +0300

    Refactor smth.

--- src/main/kotlin/lesson1/Lesson1-Task2.kt
+++ src/main/kotlin/lesson1/Lesson1-Task2.kt
@@ -1,11 +1,14 @@
 package lesson1
 fun main() {
-    val numberOfOrders: Int = 75
-    val thankYouMessage: String = "Total ordered tool sets.Thank you for your purchases!"
-    println(numberOfOrders)
-    println(thankYouMessage)
-    val stringValue1: String = "Employee count 2000"
-    //println(stringValue1)
-    val stringValue2: String = "Employee count 1999"
-    println(stringValue2)
+    val numberOfOrders: String = "Total ordered tool sets. 75"
+    val thankYouMessage: String = "Thank you for your purchases!"
+
+      println(numberOfOrders)
+      println(thankYouMessage)
+
+    var numberOfWorkers = "Employee count 2000"
+    //println(numberOfWorkers)
+
+    numberOfWorkers = "Employee count 1999"
+    println(numberOfWorkers)
 }
