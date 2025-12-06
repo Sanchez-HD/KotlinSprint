23ec122684b0de86f36d63cc4363bfc96b3a52ee
Sanchez-HD <Sanchez@>
2025-12-06 12:18:09 +0300

    KS-1-4

---
+++ src/main/kotlin/lesson1-task4.kt
@@ -0,0 +1,12 @@
+package org.example
+fun main() {
+
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short  = 6468
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("$length\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+}
